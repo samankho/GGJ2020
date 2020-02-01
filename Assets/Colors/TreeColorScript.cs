c36ef66a0a935ce338417c4ad9ea627f8b944046
Floriii <Flo@flo.de>
2020-02-01 13:13:02 +0100

    inital commit

---
+++ Assets/Colors/TreeColorScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeColorScript : MonoBehaviour
+{
+    private MeshRenderer treeColor;
+    // Start is called before the first frame update
+    void Start()
+    {
+       treeColor =  gameObject.GetComponent<MeshRenderer>();
+        treeColor.material.color = Color.green;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Cube1Script.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube1Script : MonoBehaviour
+{
+	public GameObject blueCube;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		transform.Translate(-3 * Time.deltaTime, 0, 0);
+        blueCube.transform.Rotate(50 * Time.deltaTime, 0, 0);
+    }
+}
