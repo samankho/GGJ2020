62f84ba7880767507125fa1747f25eb206beb5f7
Saman <samankh@web.de>
2020-02-02 15:16:53 +0100

    added maxscore, gameover, difficulty

--- Assets/Scripts/MoveCube.cs
+++ Assets/Scripts/MoveCube.cs
@@ -1,32 +1,65 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class MoveCube : MonoBehaviour
 {
+    
     private TextMesh textObject;
     private TextMesh text;
 
-    private static int _score = 1;
+    private static long _score = 16;
+    private static long _maxscore = 16;
+    private static bool Gameover = false;
+
     private float speed = 32;    // Start is called before the first frame update
+    private float _startZ;
+    private const float _playerZ = 500f;
+
+    private ISet<GameObject> _alreadyCollided = new HashSet<GameObject>();
+
+    public static long Score {
+        get => _score; 
+        set {
+            _maxscore = Math.Max(value, _maxscore);
+            _score = value;
+            Gameover |= value < 0;
+        }
+    }
+
+    void UpdateScoreText() {
+        if(Gameover)
+            textObject.text = "GAMEOVER";
+        else
+            textObject.text = "Score: " + Score  +"\n MaxScore:" + _maxscore;
+    }
+
     void Start()
     {
-            textObject = GameObject.Find("ScoreText").GetComponent<TextMesh>();
+        textObject = GameObject.Find("ScoreText").GetComponent<TextMesh>();
+        UpdateScoreText();
+        _startZ = transform.position.z;
     }
 
     // Update is called once per frame
     void Update()
     {
-        gameObject.transform.Translate(0, 0, -1 * Time.deltaTime * speed);
+        gameObject.transform.Translate(0, 0, -1 * Time.deltaTime * speed - ((SpawnScript.Difficulty * SpawnScript.Difficulty) / 4096f));
+        var hue = ((Math.Max(0, 10 * (transform.position.z - _playerZ) / (_startZ - _playerZ)) * 100000) % 100000) / 100000;
+        gameObject.GetComponent<MeshRenderer>().material.color = Color.HSVToRGB(hue, 1f, 1f);
     }
 
     private void OnCollisionEnter(Collision other) 
     {
-        gameObject.transform.position = new Vector3(0, 0, -350);
-        Debug.Log("Kaputttttt");
-        textObject.text = "Score: " + _score;
-        _score++;
+        if (_alreadyCollided.Contains(gameObject)) return;
 
+        _alreadyCollided.Add(gameObject);
+        
+        gameObject.transform.position = new Vector3(0   , 0, -350);
+        UpdateScoreText();
+
+        Score += 10 * (SpawnScript.Difficulty / 5);
     }
 
 }
--- Assets/Scripts/SpawnScript.cs
+++ Assets/Scripts/SpawnScript.cs
@@ -7,7 +7,12 @@ using UnityEngine;
 public class SpawnScript : MonoBehaviour {
 	public GameObject shootCube;
     public List<GameObject> shootCubes = new List<GameObject>();
+	private int _x = 0;
     private long _lastSecond = 1;
+
+    private static long _difficulty = 1;
+
+    public static long Difficulty => _difficulty;
 
     void Start() {
         InvokeRepeating("EverySecond", 0f, 0.5f);
@@ -18,24 +23,26 @@ public class SpawnScript : MonoBehaviour {
     void EverySecond() {
         float x, y;
         do {
-            x = UnityEngine.Random.Range(-7, 7);
-            y = UnityEngine.Random.Range(0.5f, 5);
+            x = UnityEngine.Random.Range(-5, 5);
+            y = UnityEngine.Random.Range(0.5f, 4);
         } while (IsNearPlayer(x, y));
         
         var z = 0;
         var gameObj = Instantiate(shootCube, new Vector3(x + 500, y, 650), Quaternion.identity);
         shootCubes.Add(gameObj);
-    }
 
-    // Update is called once per frame
+        _difficulty++;
+    }	
+
     void Update() {
         const float destroyWall = 450f;
         var toBeDestroyed = shootCubes.Where(obj => obj.transform.position.z < destroyWall)
                                       .ToList();
-
+        var malus = toBeDestroyed.Count; 
         toBeDestroyed.ForEach(Destroy);
         shootCubes = shootCubes.Where(obj => obj.transform.position.z >= destroyWall).ToList();
+
+        MoveCube.Score -= malus;
     }
 	
-
 }
